ecab30f471140e6d25025c94bb5c0d32c285f3e7
Laimacom <laima.grivaciauskaite@gmail.com>
2019-02-16 11:58:57 +0200

    Su Bootstrap

--- views.php
+++ views.php
@@ -1,67 +1,77 @@
-<?php
-function showTable($conn){
-$page = 5;
-if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-}
-if ($page < 5) $page = 5;
-if (isset($_GET['offset'])) {
-    $offset = $_GET['offset'];
-} else {
-    $offset = 0;
-}
-$sql = 'SELECT `id`, `date`, `client`, `hairdresser` FROM reservations ORDER BY `date` ASC LIMIT ' . ($page + 1) . ' OFFSET ' . $offset;
-if (!($result = $conn->query($sql))) {
-    die("Error: " . $conn->error);
-}
-if ($result->num_rows > 0) {
-?>
-<?php if ($offset > 0): ?>
-    <a href="<?= "?offset=".($offset >= $page ? $offset - $page : 0) ?>">Atgal</a>
-<?php endif; ?>
-
-<?php if ($result->num_rows == $page + 1): ?>
-    <a href="<?= "?offset=".($offset + $page) ?>">Pirmyn</a>
-<?php endif; ?>
-
-<table border=1>
-    <tr>
-        <th>ID</th>
-        <th>Data</th>
-        <th>Klientas</th>
-        <th>Kirpėja</th>
-        <th></th>
-    </tr>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rezervacijos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+</head>
+<body>
     <?php
-    for ($i = 0; $i < $page; $i++) {
-        if (!($row = $result->fetch_assoc())) break;
-        ?>
-        <tr>
-            <td><?php echo $row['id']; ?></td>
-            <td><?php echo $row['date']; ?></td>
-            <td><?php echo $row['client']; ?></td>
-            <td><?php echo $row['hairdresser']; ?></td>
-            <td><a href="?delete=<?= $row['id'] ?>" onclick="return confirm('Ar tikrai norite atšaukti rezervaciją?')">Atšaukti rezervaciją</a></td>
-        </tr>
-        <?php
+    function showTable($conn){
+    $page = 5;
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
     }
-    echo '</table>';
+    if ($page < 5) $page = 5;
+    if (isset($_GET['offset'])) {
+        $offset = $_GET['offset'];
     } else {
-        echo 'nėra duomenų';
+        $offset = 0;
     }
-    $conn->close();
+    $sql = 'SELECT `id`, `date`, `client`, `hairdresser` FROM reservations ORDER BY `date` ASC LIMIT ' . ($page + 1) . ' OFFSET ' . $offset;
+    if (!($result = $conn->query($sql))) {
+        die("Error: " . $conn->error);
+    }
+    if ($result->num_rows > 0) {
     ?>
-<?php }
+    <?php if ($offset > 0): ?>
+        <a href="<?= "?offset=".($offset >= $page ? $offset - $page : 0) ?>">Atgal</a>
+    <?php endif; ?>
 
-function forma(){?>
-    <label>Įveskite naują rezervaciją</label>
-    <form method="post">
-    <label>Data</label><input type="text" name="date" required><br>
-    <label>Klientas</label><input type="text" name="client" required><br>
-    <label>Kirpėja</label><input type="text" name="hairdresser" required><br>
-    <br>
-    <input type="submit" value="Rezervuoti">
-    </form>
-    <?php
-}
+    <?php if ($result->num_rows == $page + 1): ?>
+        <a href="<?= "?offset=".($offset + $page) ?>">Pirmyn</a>
+    <?php endif; ?>
+
+    <table class=table>
+        <tr class="table-success">
+            <th>ID</th>
+            <th>Data</th>
+            <th>Klientas</th>
+            <th>Kirpėja</th>
+            <th></th>
+        </tr>
+
+        <?php
+        for ($i = 0; $i < $page; $i++) {
+            if (!($row = $result->fetch_assoc())) break;
+            ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['date']; ?></td>
+                <td><?php echo $row['client']; ?></td>
+                <td><?php echo $row['hairdresser']; ?></td>
+                <td><a href="?delete=<?= $row['id'] ?>" onclick="return confirm('Ar tikrai norite atšaukti rezervaciją?')">Atšaukti rezervaciją</a></td>
+            </tr>
+            <?php
+        }
+        echo '</table>';
+        } else {
+            echo 'nėra duomenų';
+        }
+        $conn->close();
+    }
+
+    function forma(){?>
+        <label>Įveskite naują rezervaciją</label>
+        <form method="post">
+        <label>Data</label><input type="text" name="date" required><br>
+        <label>Klientas</label><input type="text" name="client" required><br>
+        <label>Kirpėja</label><input type="text" name="hairdresser" required><br>
+        <br>
+        <input type="submit" value="Rezervuoti">
+        </form>
+    <?php } ?>
+</body>
+</html>
